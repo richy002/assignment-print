201026a2c484964f47b8d7ae3a43d122d63f8c65
unknown <richardsonokiti@gmail.com>
2022-07-28 19:50:13 +0100

    first commit

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+
+function assignment1(){
+    for(let i = 1 ; i <= 200; i++ ){
+        if (i % 6 == 0 && i % 8 == 0){
+            console.log("Shams Tiidelab")
+        }
+        else if (i % 6 == 0){
+            console.log("Shams")
+        }
+    
+        else if (i % 8 == 0){
+            console.log("Tiidelab")
+        }else{
+            console.log(i)
+        }
+        
+    }
+ }
+
+ assignment1()
+
+
+
+ 
+
+
